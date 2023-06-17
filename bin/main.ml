163f8d3e209d8ddd98bcc45e2ded7d5266d952ba
Gavin Mason <github@gavinm.us>
2023-06-17 18:04:16 -0400

    Rearranged Config

--- bin/main.ml
+++ bin/main.ml
@@ -254,35 +254,17 @@ let bot_ai () : Sprite.action_t list = []
 module Config = struct
 	open Yojson.Basic.Util
 
+	let direction_of_bool config = if config then Right else Left
+
 	type sprite_type_config_t =
 		| Player
 		| Bot of int (*builtin ai programs*)
 		| Other
 
-	let sprite_type_config_of_json json =
-		match json |> member "type" |> to_string with
-		| "Player" -> Player
-		| "Bot" -> Bot (json |> member "Bot" |> to_int)
-		| _ -> Other
-
-	let sprite_type_of_config config : Sprite.type_t =
-		match config with
-		| Player -> Player
-		| Bot _ -> Bot bot_ai
-		| Other -> Other
-
 	type vector2_config_t = {
 		x: int;
 		y: int
 	}
-
-	let vector2_config_of_json json : vector2_config_t = {
-		x = json |> member "x" |> to_int;
-		y = json |> member "y" |> to_int
-	}
-
-	let vector2_of_config config = Vector2.create
-		(float_of_int config.x) (float_of_int config.y)
 
 	type rectangle_config_t = {
 		x: int;
@@ -291,16 +273,68 @@ module Config = struct
 		height: int;
 	}
 
+	type sprite_config_t = {
+		name: string;
+		team: int;
+		sprite_type: sprite_type_config_t;
+		src_rects: rectangle_config_t list;
+		src_rect: int;
+		dest_rect: rectangle_config_t;
+		visible: bool;
+		z_index: int;
+		use_scene_spritesheet: bool;
+		stats_constants: Sprite.stats_constant_t;
+		stats_change_on_level: Sprite.stats_change_on_level_t
+	}
+
+	type sprite_action_config_t =
+		| ShortAttack of float
+		| LongAttack of bool (*true=right, false=left*)
+		| Jump
+		| StopMoving
+		| Move of bool
+		| Speak of (string * int option) (*contents, allow_input*)
+		| EnterScene of vector2_config_t
+		| Die
+		| ExitScene
+		| NoAction
+
+	type scene_contents_config_t =
+		| Planned of (int * string * sprite_action_config_t) list
+		| Fight
+
+	type scene_config_t = {
+		spritesheet: int;
+		contents: scene_contents_config_t;
+		next_scene: int option
+	}
+
+	type t = {
+		sprites: sprite_config_t list;
+		spritesheet: string;
+		scene_spritesheets: string list;
+		scenes: scene_config_t list; (* first scene is index 0 *)
+		font: string;
+		font_ratio: float
+	}
+
+	let sprite_type_config_of_json json =
+		match json |> member "type" |> to_string with
+		| "Player" -> Player
+		| "Bot" -> Bot (json |> member "Bot" |> to_int)
+		| _ -> Other
+
+	let vector2_config_of_json json : vector2_config_t = {
+		x = json |> member "x" |> to_int;
+		y = json |> member "y" |> to_int
+	}
+
 	let rectangle_config_of_json json : rectangle_config_t = {
 		x = json |> member "x" |> to_int;
 		y = json |> member "y" |> to_int;
 		width = json |> member "width" |> to_int;
 		height = json |> member "height" |> to_int
 	}
-
-	let rectangle_of_config config = Rectangle.create
-		(float_of_int config.x) (float_of_int config.y)
-		(float_of_int config.width) (float_of_int config.height)
 
 	let stats_constant_t_of_json json : Sprite.stats_constant_t = {
 		gravity_accel = json |> member "gravity_accel" |> to_float;
@@ -322,20 +356,6 @@ module Config = struct
 		energy = json |> member "energy" |> to_int
 	}
 
-	type sprite_config_t = {
-		name: string;
-		team: int;
-		sprite_type: sprite_type_config_t;
-		src_rects: rectangle_config_t list;
-		src_rect: int;
-		dest_rect: rectangle_config_t;
-		visible: bool;
-		z_index: int;
-		use_scene_spritesheet: bool;
-		stats_constants: Sprite.stats_constant_t;
-		stats_change_on_level: Sprite.stats_change_on_level_t
-	}
-
 	let sprite_config_of_json json : sprite_config_t = {
 		name = json |> member "name" |> to_string;
 		team = json |> member "team" |> to_int;
@@ -352,6 +372,64 @@ module Config = struct
 		stats_change_on_level = json |> member "stats_change_on_level"
 			|> stats_change_on_level_t_of_json
 	}
+
+	let sprite_action_config_of_json json : sprite_action_config_t =
+		match json |> member "type" |> to_string with
+		| "ShortAttack" -> ShortAttack (json |> member "ShortAttack" |> to_float)
+		| "LongAttack" -> LongAttack (json |> member "LongAttack" |> to_bool)
+		| "Jump" -> Jump
+		| "StopMoving" -> StopMoving
+		| "Move" -> Move (json |> member "Move" |> to_bool)
+		| "Speak" ->
+				let speak = json |> member "Speak" in
+				Speak
+					(speak |> member "contents" |> to_string,
+					speak |> member "allow_input" |> to_int_option)
+		| "EnterScene" -> EnterScene (json |> member "EnterScene" |> vector2_config_of_json)
+		| "Die" -> Die
+		| "ExitScene" -> ExitScene
+		| _ -> NoAction
+
+	let scene_contents_config_of_json json : scene_contents_config_t =
+		match json |> member "type" |> to_string with
+		| "Planned" -> Planned
+			(json |> member "Planned" |> to_list |> List.map (fun elem ->
+				(elem |> member "tick" |> to_int,
+				elem |> member "sprite" |> to_string,
+				elem |> member "action" |> sprite_action_config_of_json)))
+		| _ -> Fight
+
+	let scene_config_of_json json : scene_config_t = {
+		spritesheet = json |> member "spritesheet" |> to_int;
+		contents = json |> member "contents"
+			|> scene_contents_config_of_json;
+		next_scene = json |> member "next_scene" |> to_int_option
+	}
+
+	let of_json json = {
+		sprites = json |> member "sprites" |> to_list
+			|> List.map sprite_config_of_json;
+		spritesheet = json |> member "spritesheet" |> to_string;
+		scene_spritesheets = json |> member "scene_spritesheets"
+			|> to_list |> List.map to_string;
+		scenes = json |> member "scenes" |> to_list
+			|> List.map scene_config_of_json;
+		font = json |> member "font" |> to_string;
+		font_ratio = json |> member "font_ratio" |> to_float
+	}
+
+	let sprite_type_of_config config : Sprite.type_t =
+		match config with
+		| Player -> Player
+		| Bot _ -> Bot bot_ai
+		| Other -> Other
+
+	let vector2_of_config (config: vector2_config_t) = Vector2.create
+		(float_of_int config.x) (float_of_int config.y)
+
+	let rectangle_of_config config = Rectangle.create
+		(float_of_int config.x) (float_of_int config.y)
+		(float_of_int config.width) (float_of_int config.height)
 
 	let sprite_of_config config : Sprite.t = {
 		name = config.name;
@@ -379,37 +457,6 @@ module Config = struct
 		}
 	}
 
-	let direction_of_bool config = if config then Right else Left
-
-	type sprite_action_config_t =
-		| ShortAttack of float
-		| LongAttack of bool (*true=right, false=left*)
-		| Jump
-		| StopMoving
-		| Move of bool
-		| Speak of (string * int option) (*contents, allow_input*)
-		| EnterScene of vector2_config_t
-		| Die
-		| ExitScene
-		| NoAction
-
-	let sprite_action_config_of_json json : sprite_action_config_t =
-		match json |> member "type" |> to_string with
-		| "ShortAttack" -> ShortAttack (json |> member "ShortAttack" |> to_float)
-		| "LongAttack" -> LongAttack (json |> member "LongAttack" |> to_bool)
-		| "Jump" -> Jump
-		| "StopMoving" -> StopMoving
-		| "Move" -> Move (json |> member "Move" |> to_bool)
-		| "Speak" ->
-				let speak = json |> member "Speak" in
-				Speak
-					(speak |> member "contents" |> to_string,
-					speak |> member "allow_input" |> to_int_option)
-		| "EnterScene" -> EnterScene (json |> member "EnterScene" |> vector2_config_of_json)
-		| "Die" -> Die
-		| "ExitScene" -> ExitScene
-		| _ -> NoAction
-
 	let scene_action_of_config config : Sprite.action_t = 
 		match config with
 		| ShortAttack angle -> ShortAttack angle
@@ -423,19 +470,6 @@ module Config = struct
 		| ExitScene -> ExitScene
 		| NoAction -> NoAction
 
-	type scene_contents_config_t =
-		| Planned of (int * string * sprite_action_config_t) list
-		| Fight
-
-	let scene_contents_config_json json : scene_contents_config_t =
-		match json |> member "type" |> to_string with
-		| "Planned" -> Planned
-			(json |> member "Planned" |> to_list |> List.map (fun elem ->
-				(elem |> member "tick" |> to_int,
-				elem |> member "sprite" |> to_string,
-				elem |> member "action" |> sprite_action_config_of_json)))
-		| _ -> Fight
-
 	let scene_contents_of_config config : scene_contents_t =
 		match config with
 		| Planned li ->
@@ -447,46 +481,10 @@ module Config = struct
 				Planned hashtbl
 		| Fight -> Fight
 
-
-	type scene_config_t = {
-		spritesheet: int;
-		contents: scene_contents_config_t;
-		next_scene: int option
-	}
-
-	let scene_config_of_json json : scene_config_t = {
-		spritesheet = json |> member "spritesheet" |> to_int;
-		contents = json |> member "contents"
-			|> scene_contents_config_json;
-		next_scene = json |> member "next_scene" |> to_int_option
-	}
-
-	let scene_of_config config : scene_t = {
+	let scene_of_config (config: scene_config_t) : scene_t = {
 		spritesheet = config.spritesheet;
 		contents = scene_contents_of_config config.contents;
 		next_scene = config.next_scene
-	}
-
-	type t = {
-		sprites: sprite_config_t list;
-		spritesheet: string;
-		scene_spritesheets: string list;
-		scenes: scene_config_t list; (* first scene is index 0 *)
-		font: string;
-		font_ratio: float
-	}
-
-	(*let title = json |> member "title" |> to_string in*)
-	let of_json json = {
-		sprites = json |> member "sprites" |> to_list
-			|> List.map sprite_config_of_json;
-		spritesheet = json |> member "spritesheet" |> to_string;
-		scene_spritesheets = json |> member "scene_spritesheets"
-			|> to_list |> List.map to_string;
-		scenes = json |> member "scenes" |> to_list
-			|> List.map scene_config_of_json;
-		font = json |> member "font" |> to_string;
-		font_ratio = json |> member "font_ratio" |> to_float
 	}
 
 	let to_game config : game_t =
@@ -727,9 +725,10 @@ let save_game_data (_: game_t) = ()
 let setup () =
 	init_window 1280 720 "English Final";
 	set_target_fps 60;
-	let filename = Array.get Sys.argv 1 in
-	let json = Yojson.Basic.from_file filename in
-	json |> Config.of_json |> Config.to_game
+	Array.get Sys.argv 1
+		|> Yojson.Basic.from_file
+		|> Config.of_json
+		|> Config.to_game
 
 let rec loop game =
 	match window_should_close () || game.should_exit with
